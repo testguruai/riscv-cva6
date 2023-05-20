85b098b2de2642895b6ce8302254f141f017de5e
TestGuruAI <132442834+testguruai@users.noreply.github.com>
2023-05-20 02:20:53 -0700

    Update explain_ariane.sv

--- core/ariane/explain_ariane.sv
+++ core/ariane/explain_ariane.sv
@@ -1,10 +1,12 @@
 # VerifAI TestGuru
 # Explanation for: ariane.sv
 This is a Verilog code for the top-level module of the Ariane RISC-V processor. 
-It defines a module named "ariane" and imports various packages, including the Ariane package and the CVXIF package. 
-The module has a number of input and output ports, including clock and reset signals, interrupt inputs, 
-and memory request and response signals. 
+It defines a module named "ariane" and imports various packages, 
+including the Ariane package and the CVXIF package. 
+The module has a number of input and output ports, including clock and 
+reset signals, interrupt inputs, and memory request and response signals. 
   
 The module instantiates a CVA6 module (a specific implementation of the Ariane processor), 
 and a CVXIF example coprocessor module (if CVXIF is present). 
-The module also defines a parameter ArianeCfg, which is set to the default configuration of the Ariane processor.
+The module also defines a parameter ArianeCfg, 
+which is set to the default configuration of the Ariane processor.
